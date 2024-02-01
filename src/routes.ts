716b3eb2344de50a862e908a11be97a8b7fd8f37
hang.nguyen <111733892+hangvu0420@users.noreply.github.com>
2024-02-01 12:35:55 +0700

    add router

--- src/routes.ts
+++ src/routes.ts
@@ -11,6 +11,7 @@ import GetRefCode from '@/containers/eventref/GetRefCode.vue'
 import RoboDAO from '@/containers/eventdao/RoboDAO.vue'
 import DAODetails from '@/containers/eventdao/DAODetails.vue'
 import ComingSoon from './containers/ComingSoon.vue'
+import Ethereum from '@/containers/Ethereum.vue';
 
 import Vue from "vue";
 import VueRouter from "vue-router";
@@ -37,6 +38,7 @@ const routes = [
     {path: '/vote', name: 'DAO', component: RoboDAO},
     {path: '/vote/:id', name: 'DAO Vote', component: DAODetails, props: true},
     {path: '/comingsoon', name: 'Coming Soon', component: ComingSoon},
+    {path: '/ethereum', name: 'Coming Soon', component: Ethereum},
     {path: '/403', name: 'Forbidden', component: error403},
     {path: '/500', name: 'Internal Server', component: error500},
     {path: '*', redirect: '/403', component: error403},
